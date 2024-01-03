3123fdcde6ab4f27fce704b61656feb67ef455f5
nathan nickolas <nathannickolas13@gmail.com>
2024-01-03 20:47:17 -0300

    PESSOA

---
+++ src/test/java/com/example/nathan/aula5/Aula5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nathan.aula5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
